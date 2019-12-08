3e91e6bb56545e18b950e8f82315569f8e954d08
Felipe dos Santos <felipesan.cwb@gmail.com>
2019-12-08 18:04:34 -0300

    Added dotenv

--- src/ntask.js
+++ src/ntask.js
@@ -1,11 +1,14 @@
 import TinyEmitter from 'tiny-emitter'
 import Request from 'browser-request'
+import Dotenv from 'dotenv'
+
+Dotenv.config()
 
 class Ntask extends TinyEmitter {
   constructor() {
     super()
     this.request = Request
-    this.URL = 'http://localhost:3333'
+    this.URL = process.env.BASE_URL
   }
 }
 
