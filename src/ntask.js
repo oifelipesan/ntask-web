95a8fd4961ba752f0208d3b894ed8c67597b8f81
Felipe dos Santos <felipesan.cwb@gmail.com>
2019-12-08 19:06:03 -0300

    forcing update

--- src/ntask.js
+++ src/ntask.js
@@ -8,7 +8,7 @@ class Ntask extends TinyEmitter {
   constructor() {
     super()
     this.request = Request
-    this.URL = 'https://fs-ntask-api.herokuapp.com/'
+    this.URL = 'https://fs-ntask-api.herokuapp.com'
   }
 }
 
